e645230215446291d055ff2557cb41249b404ec4
Niraj Talukdar <nirajtalukdar@gmail.com>
2018-01-23 00:43:19 +0000

    added implementation logic

--- src/com/niraj/trees/traversals/PostOrderTraversal.java
+++ src/com/niraj/trees/traversals/PostOrderTraversal.java
@@ -20,7 +20,21 @@ public class PostOrderTraversal {
 
                 Node currentNode = nodeStack.pop();
 
-                
+                if (currentNode.getLeftNode() != null && !currentNode.getLeftNode().isVisited()) {
+                    Node leftNode = currentNode.getLeftNode();
+                    leftNode.setParentNode(currentNode);
+                    nodeStack.push(leftNode);
+
+                } else if (currentNode.getRightNode() != null && !currentNode.getRightNode().isVisited()) {
+                    Node rightNode = currentNode.getRightNode();
+                    rightNode.setParentNode(currentNode);
+                    nodeStack.push(rightNode);
+
+                } else {
+                    nodeList.add(currentNode.getData());
+                    currentNode.setVisited(true);
+                    nodeStack.push(currentNode.getParentNode());
+                }
             }
         }
 
