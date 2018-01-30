7883bb6df4b6ad3cc0d7d6b3264331b321019ece
Niraj Talukdar <nirajtalukdar@gmail.com>
2018-01-30 23:18:16 +0000

    recursive implementation of lca

---
+++ src/com/niraj/trees/utitilites/LowestCommonAncestor.java
@@ -0,0 +1,33 @@
+package com.niraj.trees.utitilites;
+
+public class LowestCommonAncestor {
+
+    public Node getLCA(Node node, int dataNode1, int dataNode2) {
+
+        if (node == null) {
+            return null;
+
+        } else if (node.getData() == dataNode1 || node.getData() == dataNode2 ) {
+            return node;
+
+        }
+        Node leftNode = getLCA(node.getLeftNode(), dataNode1, dataNode2);
+        Node rightNode = getLCA(node.getRightNode(), dataNode1, dataNode2);
+
+        if (leftNode != null && rightNode != null) {
+            return node;
+        }
+
+        return (leftNode != null) ? leftNode : rightNode;
+    }
+
+    public static void main(String[] args) {
+
+        LowestCommonAncestor lca = new LowestCommonAncestor();
+        NodeInsertion nInsert = new NodeInsertion();
+        Node rootNode = nInsert.nodeGenerator();
+        Node lowestAncestor = lca.getLCA(rootNode, 7,4);
+
+        System.out.println(lowestAncestor.getData());
+    }
+}
