372c4522743fed46d1a559db601565158f2a8a8b
Niraj Talukdar <nirajtalukdar@gmail.com>
2018-01-30 00:56:40 +0000

    added another implementation of PostOrderTraversal

--- src/com/niraj/trees/traversals/PostOrderTraversal.java
+++ src/com/niraj/trees/traversals/PostOrderTraversal.java
@@ -1,6 +1,7 @@
 package com.niraj.trees.traversals;
 
 import com.niraj.trees.utitilites.Node;
+import com.niraj.trees.utitilites.NodeInsertion;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,12 @@ import java.util.Stack;
 
 public class PostOrderTraversal {
 
+    /**
+     * The below method uses a simple algorithm to do a post order traversal of a binary tree
+     * by associating the parent node and marking the visited node
+     * @param node
+     * @return java.util.List
+     */
     public List<Integer> doPostOrderTraversal(Node node) {
 
         List<Integer> nodeList = new ArrayList<>();
@@ -35,6 +42,62 @@ public class PostOrderTraversal {
                     currentNode.setVisited(true);
                     nodeStack.push(currentNode.getParentNode());
                 }
+            }
+        }
+
+        return nodeList;
+    }
+
+    /**
+     * The below algorithm does a post order traversal of a binary tree without using a parent node
+     * and without marking the visited flag
+     * @param node
+     * @return java.util.List
+     */
+    public List<Integer> postOrderTraverse(Node node) {
+
+        List<Integer> nodeList = new ArrayList<>();
+        Stack<Node> nodeStack = new Stack<>();
+        Node previousNode = null;
+
+        if (node != null) {
+
+            nodeStack.push(node);
+            while (!nodeStack.isEmpty()) {
+
+                Node currentNode = nodeStack.peek();
+
+                /** Condition to traverse in the forward direction **/
+
+                if (previousNode == null || previousNode.getLeftNode() == currentNode || previousNode.getRightNode() == currentNode) {
+
+                    if (currentNode.getLeftNode() != null) {
+                        nodeStack.push(currentNode.getLeftNode());
+
+                    } else if (currentNode.getRightNode() != null) {
+                        nodeStack.push(currentNode.getRightNode());
+
+                    } else {
+                        nodeList.add(currentNode.getData());
+                        nodeStack.pop();
+                    }
+
+                    /** Condition to traverse in the reverse direction **/
+
+                } else if (currentNode.getLeftNode() == previousNode) {
+
+                    if (currentNode.getRightNode() != null) {
+                        nodeStack.push(currentNode.getRightNode());
+                    }
+                } else {
+
+                    nodeList.add(currentNode.getData());
+                    nodeStack.pop();
+                }
+
+                /** Save the current node as the previous node to track the most recently visited node **/
+                previousNode = currentNode;
+
             }
         }
 
